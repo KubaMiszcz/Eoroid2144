e455bb690b9127e5ba11bbbc137ff610c894acce
Jakub Bełch <jbelch@dtiq.com>
2020-11-19 20:20:25 +0100

    add sonar to jenkins

---
+++ scscan.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo sonar
+
+#/srv/dev-disk-by-label-Magazyn-SSHD/SonarQube/sonar-scanner-4.5.0.2216-linux/bin/sonar-scanner -Dsonar.projectKey=Eoroid2144 -Dsonar.sources=. -Dsonar.host.url=http://192.168.1.20:9000 -Dsonar.login=c9346e8a9b4ae76f160c4f2686296779a36c747a
+
+#/srv/dev-disk-by-label-Magazyn-SSHD/SonarQube/sonar-scanner-4.5.0.2216-linux/bin/sonar-scanner -Dproject.settings=../sonar-project.properties
