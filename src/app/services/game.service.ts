f68729d014109ba71076447a902d12bff3a67b8b
Jakub Bełch <jbelch@dtiq.com>
2020-11-24 20:14:54 +0100

    apply sonar fixes

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { eGameInfo } from '../models/eGameInfo';
 import { IGameState } from '../models/IGameState';
