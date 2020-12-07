13c5695e5a4f500e91083a93f92cb5dedbe223ff
Kuba Miszcz <KubaMiszcz@users.noreply.github.com>
2020-12-07 23:11:51 +0100

    Add discord to jenkins (#18)
    * some fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import { version } from '../../package.json';
+import { version } from '../../build-info.json';
 import { buildDateTime } from '../../build-info.json';
 import { buildId } from '../../build-info.json';
 
